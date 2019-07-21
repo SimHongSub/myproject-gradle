ef939abaa62c91e5b69edd4757d76387007e8f34
Aiden <oxhe2038@naver.com>
2019-07-21 22:59:20 +0900

    project init modify.

--- myproject/src/main/java/com/aiden/myproject/ProjectInitializer.java
+++ myproject/src/main/java/com/aiden/myproject/ProjectInitializer.java
@@ -2,10 +2,15 @@ package com.aiden.myproject;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.aiden.myproject.config.ProjectConfig;
 
 public class ProjectInitializer implements WebApplicationInitializer {
 
@@ -15,11 +20,15 @@ public class ProjectInitializer implements WebApplicationInitializer {
 	}
 	
 	private void registerDispatcherServlet(ServletContext servletContext){
-		WebApplicationContext dispatcher
+		WebApplicationContext dispatcherContext = createContext(ProjectConfig.class);
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(dispatcherContext);
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("*.do");
 	}
 	
-	private WebApplicationContext createContext(Class<?>... annotatedClasses){
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+	private WebApplicationContext createContext(Class<?>...annotatedClasses){
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
 		context.register(annotatedClasses);
 		
 		return context;
