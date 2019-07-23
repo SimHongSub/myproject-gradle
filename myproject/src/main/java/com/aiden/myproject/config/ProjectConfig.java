8731bdc85a8ccafac548cece25448942d9c8dffe
Aiden <oxhe2038@naver.com>
2019-07-24 02:09:09 +0900

    ProjectConfig content add.

--- myproject/src/main/java/com/aiden/myproject/config/ProjectConfig.java
+++ myproject/src/main/java/com/aiden/myproject/config/ProjectConfig.java
@@ -2,14 +2,22 @@ package com.aiden.myproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.aiden.myproject.MainController;
 
 @Configuration
-public class ProjectConfig {
+@EnableWebMvc
+public class ProjectConfig extends WebMvcConfigurerAdapter{
 
 	/*---- ¼³Á¤ 1 ----*/
 	/*@Bean
@@ -23,9 +31,27 @@ public class ProjectConfig {
 		return new MainController();
 	}
 	
+	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry){
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
+	
+	@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+ 
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+ 
+    }
+	
+	@Bean
+    public LocaleResolver localeResolver() {
+ 
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(StringUtils.parseLocaleString("en"));
+        return cookieLocaleResolver;
+    }
 	
 	@Bean
 	public ViewResolver getViewResolver(){
